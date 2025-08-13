539901031d51dd00f2256f46989ad37187e1dad2
yeonmi0818 <parkyunme@naver.com>
2025-08-13 09:41:46 +0900

    .

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -121,7 +121,7 @@ export default function Insert(){
     }else{
       alert('로그인 성공');
       setUser(data.user); //로그인한 유저의 유저 정보 반영
-      router.refresh(); //로그인후 새로고침
+      router.refresh();//로그인후 새로고침
     }
   }
 
